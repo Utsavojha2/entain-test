bb261a5caa0a2509d54d028e542f87bd75eaf223
Utsavojha2 <utsavojha2@gmail.com>
2022-05-26 18:44:04 +0545

    Changes to test setup

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  collectCoverage: true,
+  roots: ['<rootDir>/src'],
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/src/**/*.{test}.{js,jsx,ts,tsx}',
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  resetMocks: true,
 };
